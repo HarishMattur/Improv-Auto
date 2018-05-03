ab986cdff6d67427a07eebcd3e059c2b402565b1
HarishMattur <38909791+HarishMattur@users.noreply.github.com>
2018-05-03 15:33:58 +0000

    Delete Parse_csv.py

--- Mypack/Code/Parse_csv.py
+++
@@ -1,9 +0,0 @@
-
-with open('C:\\Users\\AB17764\\Improv-Auto\\Mypack\\Supporting files\\parse.csv', newline='') as f:
-    line=f.readline()
-    unm=(line[1:8])
-    psw=(line[9:18])
-#   print(unm)
-#    print(psw)
-
-
